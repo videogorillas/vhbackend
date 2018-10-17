a46b01f93bf044206143e8e4a4663b7be0c84a82
Sevruk <aleksey.sevruk@gmail.com>
2018-10-17 07:11:01 -0700

    initial

---
+++ Dashboard/Program.cs
@@ -0,0 +1,25 @@
+﻿using Bridge.Html5;
+using Bridge.React;
+
+namespace Dashboard
+{
+    public class Program
+    {
+        static ReactElement View(string name)
+        {
+            // create a virtual div element
+            return DOM.Div($"Hello {name}");
+        }
+
+
+        public static void Main()
+        {
+            // get the element that mounts the app
+            var appRoot = Document.GetElementById("app");
+            // create the user interface
+            var userInterface = View("Mike");
+            // attach the user interface to the root node
+            React.Render(userInterface, appRoot);
+        }
+    }
+}
